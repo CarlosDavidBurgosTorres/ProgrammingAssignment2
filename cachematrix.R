68aa83625a9f66f781c6567b5b66eaa9a54bf79b
CarlosDavidBurgosTorres <cdburgos1@espe.edu.ec>
2014-04-27 19:56:29 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function creates a special object "matrix" that can cache its inverse.
 
-## Write a short comment describing this function
+## makeVector: Creates a special "vector", which is really a list containing a function to:
+## 1.- set the value of the vector
+## 2.- get the value of the vector
+## 3.- set the value of the mean
+## 4.- get the value of the mean
+
 
 makeCacheMatrix <- function(x = matrix()) {
+         i  <- NULL
+         set  <- function(y){
+                 x <<- y
+                 i <<- NULL 
+         }
+         get  <- function() x
+         setinverse  <- function(inverse) i  <<- inverse
+         getinverse  <- function() i
+         list(set= set, get = get, 
+              setinverse = setinverse, 
+              getinverse = getinverse)
+ 
+ }
 
-}
-
-
-## Write a short comment describing this function
+## cacheSolve: This function calculates the inverse of the "matrix" special makeCacheMatrix previously returned by.
+## If the reverse is already calculated (and the matrix has not changed), then the reverse cachesolve must retrieve cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+         i  <- x$getinverse()
+         if (!is.null(i)){
+                 message("receive data in cache")
+                 return(i)
+         }
+         data  <- x$get()
+         i  <- solve(data, ...)
+         x$setinverse(i)
+         i
+ }
+rows <- 5
+columns <- 5
+mtx <- makeCacheMatrix(matrix(rnorm((r*c),mean=2,sd=0.2),nrow=rows,ncol=columns))
+cacheSolve(mtx) 
+
+## rnorm: this function chooses random values ​​for the matrix
